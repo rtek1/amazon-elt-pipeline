4a9a78bf5294743da79f3d263404a7a481821991
rtek1 <richard_tek@hotmail.com>
2024-12-13 10:49:22 -0500

    Removed all files from remote repository

--- airflow/dbt_project/models/dim/dim_reviews.sql
+++
@@ -1,9 +0,0 @@
--- models/dim_reviews.sql
-SELECT
-    review_id,
-    review_title,
-    review_content
-FROM
-    {{ ref('stg_amazon_sales_data') }}
-GROUP BY
-    review_id, review_title, review_content
--- airflow/dbt_project/models/dim/dim_users.sql
+++
@@ -1,8 +0,0 @@
--- models/dim/dim_users.sql
-SELECT
-    user_id,
-    user_name
-FROM
-    {{ ref('stg_amazon_sales_data') }}
-GROUP BY
-    user_id, user_name
